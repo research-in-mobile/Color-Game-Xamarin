aa049cf75c32a249dc4d889dcaaa28a7dfc627a6
Ajay Sivanesu <contact@researchinmobile.com>
2022-09-27 12:10:20 -0400

    added Test projects

---
+++ ColorGame.FunctionalTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ColorGame.FunctionalTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
--- ColorGame/ColorGame/Services/DataService/ILocalDataService.cs
+++ ColorGame/ColorGame/Services/DataService/ILocalDataService.cs
@@ -8,12 +8,11 @@ namespace ColorGame.Services
     public interface ILocalDataService
     {
         User CurrentUser { get; }
-        ILocalDataService SetCurrentUser(User user);
+        List<ScoreCard> ActiveScoreCards { get; set; }
 
+        ILocalDataService SetCurrentUser(User user);
         ILocalDataService StoreContext();
         ILocalDataService SaveContext();
         void LoadCurrentUserScoreCards();
-
-        List<ScoreCard> ActiveScoreCards { get; set; }
     }
 }
